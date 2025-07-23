44b56618a77738cec4c084c06e7273c391633522
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2025-07-23 11:54:08 +0530

    assignment corse 2

--- Assignmentss/Course2Assignment1.java
+++ Assignmentss/Course2Assignment1.java
@@ -1,5 +1,15 @@
 package Assignmentss;
-
+import java.util.Random;
 public class Course2Assignment1 {
-
+	public static void main(String[] args) {
+		long random10DigitNumber = generateTenDigitRandomNumber();
+		System.out.println("Random 10-Digit Number: " + random10DigitNumber);
+	}
+	public static long generateTenDigitRandomNumber() {
+		Random random = new Random();
+		long min = 1000000000L;
+		long max = 9999999999L;
+		long random10DigitNumber = min + ((long) (random.nextDouble() * (max - min + 1)));
+		return random10DigitNumber;
+	}
 }
