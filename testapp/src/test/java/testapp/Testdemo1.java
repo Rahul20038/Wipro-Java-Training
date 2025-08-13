ce88a3db4c9e4d3199d50e71ea5639df55bacf03
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2025-08-13 12:06:37 +0530

    Testing codes using TestNG

---
+++ testapp/src/test/java/testapp/Testdemo1.java
@@ -0,0 +1,13 @@
+package testapp;
+
+import org.testng.annotations.Test;
+
+public class Testdemo1 {
+	@Test
+	public void add() {
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("Test NG");
+	}
+}
